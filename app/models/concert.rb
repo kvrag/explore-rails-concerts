cc91aca996ed37d44cd06258b1e14a32e4a910be
Kristina Vragovic <k.vragovic@gmail.com>
2017-07-22 11:44:38 -0500

    Concerts index view

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -3,4 +3,8 @@ class Concert < ApplicationRecord
   has_many :users, through: :attendances, source: :attendee_id
 
   validates :band, :venue, :date, :start_time, presence:true
+
+  def formatted_date
+    self.date.strftime("%m/%d/%Y")
+  end
 end
