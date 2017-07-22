c1d2af31b4005c7e1f33e505e244790692830e82
Kristina Vragovic <k.vragovic@gmail.com>
2017-07-22 12:15:28 -0500

    User signup complete

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @user.authenticate(user_params[:password])
+      redirect_to '/'
+    else
+      @errors = @user.errors.full_messages
+      render :new 
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/' => "concerts#index"
 
   resources :concerts, only: [:index, :show]
+  resources :users, only: [:new, :create]
 end
