1d0e9b95d0dd1cb3024758e3234358cfc6ae1bd2
Kristina Vragovic <k.vragovic@gmail.com>
2017-07-22 14:32:18 -0500

    Styling

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,11 +2,17 @@ class AttendancesController < ApplicationController
 
   def create
     @attendance = Attendance.new(attendance_params)
+    @concert = Concert.find(attendance_params[:concert_id])
     if @attendance.save 
-      redirect_to "/concerts/#{@attendance.concert_id}"
+      redirect_to concert_url(@concert)
     else
       @errors = @attendance.errors.full_messages
-      render concert_path(@attendance.concert)
+      render concert_path(@concert)
     end
   end
+
+  private
+    def attendance_params
+      params.require(:attendance).permit(:user_id, :concert_id)
+    end
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,5 +2,5 @@ class Attendance < ApplicationRecord
   belongs_to :attendee, class_name: 'User', foreign_key: :user_id
   belongs_to :concert
 
-  
+  validates_uniqueness_of :user_id, scope: :concert_id   
 end
