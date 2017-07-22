60f7a764afbd21ec567613c99512638c7537cfd7
Kristina Vragovic <k.vragovic@gmail.com>
2017-07-22 11:12:40 -0500

    Users model and migration

---
+++ db/migrate/20170722160855_create_concerts.rb
@@ -0,0 +1,6 @@
+class CreateConcerts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :concerts do |t|
+    end
+  end
+end
---
+++ db/migrate/20170722160902_create_attendances.rb
@@ -0,0 +1,6 @@
+class CreateAttendances < ActiveRecord::Migration[5.0]
+  def change
+    create_table :attendances do |t|
+    end
+  end
+end
