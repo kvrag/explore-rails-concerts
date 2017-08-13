f48e4fa53ffe76005b6dc861a8fc5ed4600dcb26
Kristina Vragovic <k.vragovic@gmail.com>
2017-08-13 15:30:38 -0500

    Adjust schema for concert attendees as users

--- db/migrate/20170722160902_create_attendances.rb
+++ db/migrate/20170722160902_create_attendances.rb
@@ -1,7 +1,7 @@
 class CreateAttendances < ActiveRecord::Migration[5.0]
   def change
     create_table :attendances do |t|
-      t.integer :attendee_id
+      t.integer :user_id
       t.integer :concert_id
 
       t.timestamps
